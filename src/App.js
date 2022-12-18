8e9af80ab78a214a5aee0343830f05e21fcbf47d
ellzmarie <elliematutisdigal@gmail.com>
2022-12-17 17:06:18 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import About from './pages/About.js';
+import Stocks from './pages/Stocks.js';
+import Home from './pages/Home.js';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/stocks' element={<Stocks />} />
+        <Route path='/stocks/:symbol' element={<Stocks />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+
+
+function About() {
+    return (
+      <div className="About">
+        <h4>
+            Financial Modeling Prep API provides real time stock price, company financial statements, major index prices, stock historical data, forex real time rate and cryptocurrencies. Financial Modeling Prep stock price API is in real time, the company reports can be found in quarter or annual format, and goes up to 30 years back in history.
+        </h4>
+      </div>
+    );
+  }
+  
+  export default About;
