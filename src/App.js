6534610aaf1d93107f2227d0f06cc9836887b022
ellzmarie <elliematutisdigal@gmail.com>
2022-12-18 19:24:23 -0800

    updates line 17

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/stocks' element={<Dashboard />} />
-        <Route path='/stocks/:symbol' element={<Stocks />} />
+        <Route path='/stocks/:symbol' element={<Stock />} />
 
       </Routes>
     </div>
